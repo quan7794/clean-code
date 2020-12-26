3d6eda7003f8178d2b6329dfc4661a7a6339a7eb
mtquan <quanmtfu@gmail.com>
2020-12-26 08:34:00 +0700

    Newest codes

--- cleanCode.py
+++ cleanCode.py
@@ -1,23 +1,36 @@
 
 
-#6____________________________________________________________________________________
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+#6__________Don't Pass Null.__________________________________________________________________________
+class Cat:
+    def __init__(self, name, color):
+        self.name = name
+        self.hairColor = color
 
-def demo6():
-    point1 = None
-    point2 = Point(10,15)
-    print(multiTwoPointY(point1,point2))
+def main():
+    Sphynx  = Cat("Sphynx", None)
+    FelisCatus = Cat("Felis Catus", "Bicolor")
+    print(childCatHairColor(Sphynx , FelisCatus))
 
-def multiTwoPointY(point1, point2):
-    try:
-        return (point1.y * point2.y)
-    except Exception as e:
-        print(f"Can not multi, error: {e}")
+def childCatHairColor(cat1, cat2):
+        return (cat1.hairColor + cat2.hairColor)
 
-#5____________________________________________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#5______________________Don't Return Null.______________________________________________________________
 def demo5():
     emp1 = Employee("Employee 1",18)
     emp2 = Employee("Employee 2",25)
@@ -26,7 +39,7 @@ def demo5():
     company1 = Company("Company1", [emp1,emp2,emp3])
     company2 = Company("Company2", None)
 
-    employees = getEmployees(company2)
+    employees = getEmployees(company1)
     for employee in employees:
         print(employee.name) 
 
@@ -47,51 +60,60 @@ def getEmployees(company):
         return []
 
 
-#4____________________________________________________________________________________
+
+
+
+
+
+
+
+
+#4_____________Define the Normal Flow___________________
 def demo4():
     box1 = ["cake1","cake2","cake3"]
     box2 = 10
     box3 = ["cake1","cake2","cake3","cake4","cake5"]
     sumCake = 0
     sumCake += getCakeCount(box1)
-    print(sumCake)
+    print(f"Sum cakes count after check box 1: {sumCake}")
     sumCake += getCakeCount(box2)
-    print(sumCake)
+    print(f"Sum cakes count after check box 2: {sumCake}")
     sumCake += getCakeCount(box3)
-    print(sumCake)
+    print(f"Sum cakes count after check box 3: {sumCake}")
+
 def getCakeCount(box):
     try:
         return len(box)
     except:
         return 0
-#3____________________________________________________________________________________
-def tryOpenFile(filePath):
-    try:
-        f = open(filePath, "r")
-        content = f.read()
-        f.close()
-        print(content)
-    except Exception as e:
-        createFile(filePath)
 
-def createFile(filePath):
-    return    
 
-#2____________________________________________________________________________________
+
+
+
+
+
+
+
+#2___________Extract Try/Catch blocks._________________________________________________________________________
 import datetime
+
 def updateFile(filePath, newContent):
     f = open(filePath, "a")
     f.write(f"{newContent} \n")
     f.close()
+
 def printFileContent(filePath):
     f = open(filePath, "r")
     content = f.read()
     f.close()
     print(content)
+
 def demo2():
     existFilePath = "demo/cleanCode.txt"
     notExistFilePath = "demo/myGirlFriendInfo.txt"
     path = existFilePath
+
     newContent = datetime.datetime.now()
     try:
         printFileContent(path)       # Print current file content.
@@ -100,8 +122,18 @@ def demo2():
     except Exception as e:
         print(f"Error: {e}")
 
+
+
+
+
+
+
+
+
 #1________________Use Exceptions Rather Than Return Codes____________________________________________________________________
-def main():
+import enum
+
+def demo1():
     num1 = "10" 
     num2 = "It is string."
     num3 = "0"
@@ -110,7 +142,6 @@ def main():
     result = divisionUseException(num1, num3)
     print(result)
 
-import enum
 class errorHandler(enum.Enum):
     DIVISION_BY_ZERO_ERROR = "divideByZero"
     NOT_NUMBER_ERROR = "notNumber"
